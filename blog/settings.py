cdb9be9c25f7b6e89b281c99414bece83213c81b
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-23 09:39:50 +0100

    added static root dir and a few other path

--- blog/settings.py
+++ blog/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# TEMPLATE_DIR = 'blog/template/blog'
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL ='/'
