637b869fe8a733b940662c0f00d0e4b0ddb6e713
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-23 09:59:49 +0100

    created postlist class base view and url route

--- kingblog/urls.py
+++ kingblog/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
+    path("/", PostListView.as_view(), name="post_list"),
     path("/whoami",AboutView.as_view() ,name='whoami'),
 ]
--- kingblog/views.py
+++ kingblog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from django.utils import timezone
+
+from kingblog.models import Post, Comments
 
 # Create your views here.
 
 class AboutView(TemplateView):
     template_name = "whoami.html"
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = ''
+    template_name=''
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().orderby("-published_date"))
+    
